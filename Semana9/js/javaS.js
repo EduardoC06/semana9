535c15de2cc555a018c5cff901cec3446a49851b
EduardoC06 <105380241+EduardoC06@users.noreply.github.com>
2023-10-11 16:33:35 -0500

    Add files via upload

---
+++ Semana9/js/javaS.js
@@ -0,0 +1,11 @@
+$('.btn-ver-menu') .on('click', function()){
+
+    $('.pagina-izquierda') .addClass("pagina-izquierda2");
+
+    setTimeout{function(){
+
+        $('.pagina-derecha') .addClass("pagina-derecha2")
+
+    }, 300) 
+
+}}
